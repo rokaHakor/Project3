12ccf3ea36051cd58ef476121cefb9ff7e88d7f7
Irmuun <rokahakor@gmail.com>
2020-04-13 19:31:33 -0700

    Initial Gui

--- Project3/src/main/gui/Deliverables.java
+++ Project3/src/main/gui/Deliverables.java
@@ -8,12 +8,7 @@ public class Deliverables extends JFrame {
     public Deliverables() {
         setTitle("Project Management System");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setAlwaysOnTop(true);
-        JPanel startPanel = new JPanel();
-        startPanel.setLayout(new BoxLayout(startPanel, BoxLayout.Y_AXIS));
-        startPanel.add(Box.createVerticalGlue());
-        startPanel.setPreferredSize(new Dimension(500, 600));
-        setContentPane(startPanel);
-        pack();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize((int)(screenSize.width*.8), (int)(screenSize.height*.8));
     }
 }
